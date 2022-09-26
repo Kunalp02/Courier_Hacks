01dfeec8313f2c6faf42f475bf0d4c38158ed628
Kunal Patil <kunalpatil970730@gmail.com>
2022-09-26 22:49:24 +0530

    Commmit

--- shop/settings.py
+++ shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,4 +175,4 @@ RAZORPAY_KEY_SECRET = config('RAZORPAY_KEY_SECRET', default='')
 
 
 
-
+django_heroku.settings(locals())
